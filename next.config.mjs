217353ebf565ef4aeee9a4e52063d65b958762fc
EEVEE4U <143544942+EEVEE4U@users.noreply.github.com>
2024-07-16 23:37:53 +0530

    unoptimised images

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
-        port: '',
-        pathname: '**',
-      },
-    ],
-  },
+    unoptimized: false,
+  }
 };
 
 export default nextConfig;
